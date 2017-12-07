ee958fd8d330ba4ec771cefcde39c4d94041fc44
Jonah Kim <jonah.kim@warriorlife.net>
2017-12-07 14:56:20 -0800

    js part of calculator

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+//change display
+function d(val) {
+		document.getElementById("d").value = val;
+}
+
+//Type numbers and operators
+function v(val) {
+		document.getElementById("d").value += val;
+}
+
+//evaluate the equation
+function e() {
+		try {
+			  d(eval(document.getElementById("d").value));
+		}
+		catch(err) {
+			d("Error");
+		}
+}
